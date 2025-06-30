1e23c9a54e7b0b08f42d20c74adc1da68faaf60e
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-06-30 13:02:54 -0400

    Probando con los meses del año...

---
+++ Archivos Ejecutables/Arreglos/arrays.js
@@ -0,0 +1,15 @@
+/* ARREGLOS EN JAVASCRIPT! */
+
+// VARIABLES A DECLARAR:
+
+    // Tipo de Variable + Nombre de una variable = ['String', 'String', 'String', int, boolean];
+
+    // var friends = ['Carlos', 'Alejandro', 'Cesar', 27, true];
+
+    // PROBAREMOS CON LOS MESES DEL AÑO:
+
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+    // document.writeln(friends);
+    
+    document.writeln(months);
--- Archivos Ejecutables/introduccion.js
+++
@@ -1,3 +0,0 @@
-/* ARCHIVO BÁSICO PARA TRABAJAR EN JAVASCRIPT! */
-
-alert('Hola mundo, soy JavaScript desde un archivo externo en acción!');
