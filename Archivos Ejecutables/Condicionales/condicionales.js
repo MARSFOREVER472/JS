854f09f97033775ecd829ad4972e263bc597056a
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-06-30 14:42:17 -0400

    Practicando...

---
+++ Archivos Ejecutables/Condicionales/condicionales.js
@@ -0,0 +1,27 @@
+/* CONDICIONALES EN JAVASCRIPT */
+
+/* 
+   
+   SIGUIENDO ESTE ALGORITMO:
+
+   if(condicion) 
+   {
+      codigo... 
+   }
+
+*/
+
+// ENTONCES TENEMOS EL SIGUIENTE EJEMPLO:
+
+   var age = 15;
+
+   if (age >= 18)
+   {
+    document.write('ERES MAYOR DE EDAD');
+   }
+
+   else
+   {
+    document.write('ERES MENOR DE EDAD');
+   }
+
