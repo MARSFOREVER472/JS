032c709666f951d32d3b32db6d8615bf26eb63b2
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-06-30 13:11:54 -0400

    Probando con algunos meses mediante un index!!!

--- Archivos Ejecutables/Arreglos/arrays.js
+++ Archivos Ejecutables/Arreglos/arrays.js
@@ -6,10 +6,25 @@
 
     // var friends = ['Carlos', 'Alejandro', 'Cesar', 27, true];
 
-    // PROBAREMOS CON LOS MESES DEL AÑO:
+    // PROBAREMOS CON LOS MESES DEL AÑO (EL INDEX SE INICIALIZA EN 0 DENTRO DE UN ARREGLO):
 
     var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
     // document.writeln(friends);
-    
-    document.writeln(months);
+
+    document.writeln(months[3]);
+    /* 
+
+       document.writeln(months[1]);
+       document.writeln(months[2]);
+       document.writeln(months[3]);
+       document.writeln(months[4]);
+       document.writeln(months[5]);
+       document.writeln(months[6]);
+       document.writeln(months[7]);
+       document.writeln(months[8]);
+       document.writeln(months[9]);
+       document.writeln(months[10]);
+       document.writeln(months[11]);
+
+    */
