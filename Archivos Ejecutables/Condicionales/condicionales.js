6f162cb2b6e91c78c1e7f73399000c1075aa0170
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-06-30 14:54:31 -0400

    Probando con algunas condicionales especiales!!!!

--- Archivos Ejecutables/Condicionales/condicionales.js
+++ Archivos Ejecutables/Condicionales/condicionales.js
@@ -13,6 +13,8 @@
 
 // ENTONCES TENEMOS EL SIGUIENTE EJEMPLO:
 
+/*
+
    var age = 15;
 
    if (age >= 18)
@@ -25,3 +27,51 @@
     document.write('ERES MENOR DE EDAD');
    }
 
+*/
+
+   // TRABAJANDO CON CONDICIONALES BÁSICAS...
+
+   /*
+
+   var pais = 'Chile';
+
+   if (pais == 'Mexico')
+   {
+    document.write('ERES UN COMPATRIOTA DE ESTE PAÍS!!!!');
+   }
+
+   else if(pais == 'España')
+   {
+    document.write('ERES UN INMIGRANTE REGULAR DE SU PAÍS!!!!');
+   }
+
+   else
+   {
+    document.write('ERES UNA PERSONA EXTRANJERA!!!!');
+   }
+
+   */
+
+   // TRABAJANDO MEDIANTE UN "switch"...
+
+   var pais = 'Chile';
+
+   switch(pais)
+   {
+
+    case 'Mexico':
+        document.write('ERES MEXICANO!!!!');
+        break;
+
+    case 'España':
+        document.write('ERES ESPAÑOL!!!!');
+        break;
+
+    case 'Colombia':
+        document.write('ERES COLOMBIANO!!!!');
+        break;
+
+    default:
+        document.write('ERES UNA PERSONA EXTRANJERA!!!!');
+        break;
+   }
