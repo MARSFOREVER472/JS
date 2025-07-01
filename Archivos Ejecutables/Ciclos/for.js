3330e2fc67babfff3c8b09d6ca1278203eab05e5
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-07-01 14:20:51 -0400

    Mostrando valores usando el ciclo "for"!

--- Archivos Ejecutables/Ciclos/for.js
+++ Archivos Ejecutables/Ciclos/for.js
@@ -1,8 +1,29 @@
 /* CICLO FOR */
 
-document.write('Hello!');
-document.write('Hello!');
-document.write('Hello!');
-document.write('Hello!');
-document.write('Hello!');
-document.write('Hello!');
+/*
+
+document.write('1');
+document.write('2');
+document.write('3');
+document.write('4');
+document.write('5');
+document.write('6');
+
+*/
+
+// ESTRUCTURA BÁSICA DE UN CICLO "for":
+
+/*
+
+for (inicio; condicion; actualizacion)
+{
+    // Código...
+}
+
+*/
+
+for (var i = 1; i < 10; i++)
+{
+    document.write('Hello!');
+    document.write('<br>');
+}
