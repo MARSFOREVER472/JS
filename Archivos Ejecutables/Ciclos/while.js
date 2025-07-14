c4d1b860521c4b5c02255ff445a4358cc9e8d7a0
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-07-14 10:07:04 -0400

    Ciclo "while" agregado al proyecto!!!!

---
+++ Archivos Ejecutables/Ciclos/while.js
@@ -0,0 +1,20 @@
+// CICLO "while"
+
+/*
+
+while(condicion)
+{
+    // Código...
+}
+
+*/
+
+var number = 1;
+
+while(number <= 10)
+{
+
+    document.write(number + '<br>');
+    number = number + 1;
+
+}
