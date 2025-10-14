70e9edcebba3b342a5d93bad567d4eed916d1feb
Marcelo Andrés <69094327+MARSFOREVER472@users.noreply.github.com>
2025-10-14 12:40:59 -0300

    Ejercicio 3 concluido!!!!!

--- Archivos Ejecutables/EjerciciosPropuestos/ejerciciosPropuestos.js
+++ Archivos Ejecutables/EjerciciosPropuestos/ejerciciosPropuestos.js
@@ -44,3 +44,29 @@
    dress.prize = parseInt(prompt("Precio Bruto: "));
    dress.discount = parseInt(prompt("Precio Bruto: "));
    console.log("El Precio Neto es de: " + dress.neto() + " €");
+
+   // EJERCICIO 3:
+
+   // LO QUE SE PIDE: En esta propuesta debes crear una clase de nombre piedra (con class o con function) con dos propiedades: masa, volumen y un método densidad que calcule este valor (masa/volumen). Luego instancia al menos un objeto de esta clase, dale un valor a su masa y a su volumen y ejecuta el método densidad.
+
+   // ACLARACIONES: Consta de la clase objeto "oro", "masa de la muestra" 194 gr y "volumen" 10 cc. "Densidad" deberá devolver 19,4.
+
+   // SOLUCIÓN:
+
+   class rock
+   {
+      constructor(m, v)
+      {
+         this.masa = m;
+         this.volumen = v;
+      }
+
+      densidad = function()
+      {
+         return this.masa / this.volumen;
+      }
+   }
+
+   var gold = new rock(194, 10);
+
+   console.log("Densidad: " + gold.densidad());
